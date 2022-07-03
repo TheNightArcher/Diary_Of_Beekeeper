6934c66ab2b6ef2c50d9c212a46f4283155ca01d
Galin Manov <thenightarchervclaymore@gmail.com>
2022-07-03 14:03:02 +0300

    Add a ModelMapper

---
+++ src/main/java/bg/dirybeekeeper/diaryofbeekeeper/DiaryOfBeekeeperApplication.java
@@ -0,0 +1,13 @@
+package bg.dirybeekeeper.diaryofbeekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryOfBeekeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryOfBeekeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/dirybeekeeper/diaryofbeekeeper/DiaryOfBeekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.dirybeekeeper.diaryofbeekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryOfBeekeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
