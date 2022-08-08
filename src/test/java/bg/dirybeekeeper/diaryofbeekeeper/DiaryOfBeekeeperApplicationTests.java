00f99810eebe26ef57ea076fd5137d4430c65adb
Galin Manov <thenightarchervclaymore@gmail.com>
2022-08-08 13:53:37 +0300

    Did some changes

--- src/test/java/bg/dirybeekeeper/diaryofbeekeeper/DiaryOfBeekeeperApplicationTests.java
+++ src/test/java/bg/dirybeekeeper/diaryofbeekeeper/DiaryOfBeekeeperApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DiaryOfBeekeeperApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
