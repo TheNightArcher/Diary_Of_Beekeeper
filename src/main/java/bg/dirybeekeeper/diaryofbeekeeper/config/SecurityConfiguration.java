805fcc83c5824b1e9677fb1006fa5fd2b9bb44db
Galin Manov <thenightarchervclaymore@gmail.com>
2022-09-08 15:32:12 +0300

    Init

--- src/main/java/bg/dirybeekeeper/diaryofbeekeeper/config/SecurityConfiguration.java
+++ src/main/java/bg/dirybeekeeper/diaryofbeekeeper/config/SecurityConfiguration.java
@@ -27,8 +27,8 @@ public class SecurityConfiguration {
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 // everyone can login and register
                 .antMatchers("/", "/static/favicon.ico", "/users/forgot").permitAll()
-                .antMatchers("/users/login", "/users/register", "/users/process_register", "/verify", "/change").anonymous()
-                .antMatchers("/users/add-beehives", "/users/beehives/**","**/api/**").authenticated()
+                .antMatchers("/", "/users/login", "/users/register", "/users/process_register", "/verify", "/change").anonymous()
+                .antMatchers("/users/add-beehives", "/users/beehives/**", "**/api/**").authenticated()
                 .antMatchers("/users/admin/view").hasRole("ADMIN")
                 // all other pages are available for logger in users
                 .anyRequest()
