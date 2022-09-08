37da8fbfbd9714cb59fe08a283e27ec509f6c5e4
Galin Manov <thenightarchervclaymore@gmail.com>
2022-09-08 17:24:59 +0300

    Init

--- src/main/java/bg/dirybeekeeper/diaryofbeekeeper/web/HomeController.java
+++ src/main/java/bg/dirybeekeeper/diaryofbeekeeper/web/HomeController.java
@@ -2,8 +2,10 @@ package bg.dirybeekeeper.diaryofbeekeeper.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class HomeController {
 
     @GetMapping("/")
