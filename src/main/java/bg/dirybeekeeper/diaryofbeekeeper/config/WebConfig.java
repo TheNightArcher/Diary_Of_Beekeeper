b24dca164d72f6ea6c2ece61cf74e74af09a8d23
Galin Manov <thenightarchervclaymore@gmail.com>
2022-09-13 11:47:53 +0300

    Adding Template resolver

--- src/main/java/bg/dirybeekeeper/diaryofbeekeeper/config/WebConfig.java
+++ src/main/java/bg/dirybeekeeper/diaryofbeekeeper/config/WebConfig.java
@@ -1,9 +1,12 @@
 package bg.dirybeekeeper.diaryofbeekeeper.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -18,5 +21,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         WebMvcConfigurer.super.addInterceptors(registry);
         registry.addInterceptor(localeChangeInterceptor);
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver yourTemplateResolver() {
+        ClassLoaderTemplateResolver configurer = new ClassLoaderTemplateResolver();
+        configurer.setPrefix("templates/");
+        configurer.setSuffix(".html");
+        configurer.setTemplateMode(TemplateMode.HTML);
+        configurer.setCharacterEncoding("UTF-8");
+        configurer.setOrder(0);  // this is important. This way spring //boot will listen to both places 0 and 1
+        configurer.setCacheable(false);
+        configurer.setCheckExistence(true);
+        return configurer;
     }
 }
